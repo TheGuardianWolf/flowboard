ddfab99bc57a9044953469ea3b076f3ed5af9c7c
TheGuardianWolf <nano@pixelcollider.net>
2020-05-23 03:03:15 +0000

    Add page contents

--- flowboard/imports/ui/layouts/app-body.js
+++ flowboard/imports/ui/layouts/app-body.js
@@ -1,1 +1,11 @@
 import './app-body.html';
+
+import { Template } from 'meteor/templating';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
+
+Template.App_body.events({
+  'click #nav-logout': (event) => {
+    event.preventDefault();
+    AccountsTemplates.logout();
+  }
+})
---
+++ flowboard/typings/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="globals/kadira:blaze-layout/index.d.ts" />
+/// <reference path="globals/kadira:flow-router/index.d.ts" />
